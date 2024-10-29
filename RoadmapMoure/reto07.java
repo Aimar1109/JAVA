4ce0886cb2db99cdab760ee96f2515403576c001
Aimar1109 <aimar.junagore@gmail.com>
2024-10-29 22:28:17 +0100

    reto07 empezando

---
+++ RoadmapMoure/reto07.java
@@ -0,0 +1,17 @@
+import java.util.Stack;
+
+public class reto07 {
+    public static void main(String[] args) {
+        // PILAS Y COLAS
+
+        // Pilas
+        Stack<Integer> pila = new Stack<>();
+        pila.push(1);
+        pila.push(3);
+        pila.push(2);
+
+        // System.out.println(pila.remove(0));
+        System.out.println(pila);
+
+    }
+}
